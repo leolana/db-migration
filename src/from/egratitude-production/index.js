71c8e28e3819e2ef872c5439879752c0a07eaa53
Leonardo Lana <leolucas.lana@hotmail.com>
2022-06-27 12:30:26 -0300

    feat: add interface to query one record

--- src/from/egratitude-production/index.js
+++ src/from/egratitude-production/index.js
@@ -1,17 +1,41 @@
-import { queryToMigrate as activityQueryMigrate } from "./activities";
-import { queryToMigrate as classroomQuery } from "./classrooms";
-import { queryToMigrate as courseQuery } from "./courses";
-import { queryToMigrate as groupQuery } from "./groups";
-import { queryToMigrate as statQuery } from "./stat";
-import { queryToMigrate as userQuery } from "./users";
+import {
+  queryToMigrate as activityQueryMigrate,
+  queryOneToMigrate as activityQueryOneMigrate,
+} from "./activities";
+import {
+  queryToMigrate as classroomQuery,
+  queryOneToMigrate as classroomQueryOne,
+} from "./classrooms";
+import {
+  queryToMigrate as courseQuery,
+  queryOneToMigrate as courseQueryOne,
+} from "./courses";
+import {
+  queryToMigrate as groupQuery,
+  queryOneToMigrate as groupQueryOne,
+} from "./groups";
+import {
+  queryToMigrate as statQuery,
+  queryOneToMigrate as statQueryOne,
+} from "./stat";
+import {
+  queryToMigrate as userQuery,
+  queryOneToMigrate as userQueryOne,
+} from "./users";
 
 const egratitudeProduction = {
   activityQuery: activityQueryMigrate,
+  activityQueryOne: activityQueryOneMigrate,
   classroomQuery: classroomQuery,
+  classroomQueryOne: classroomQueryOne,
   courseQuery: courseQuery,
+  courseQueryOne: courseQueryOne,
   groupQuery: groupQuery,
+  groupQueryOne: groupQueryOne,
   statQuery: statQuery,
+  statQueryOne: statQueryOne,
   userQuery: userQuery,
+  userQueryOne: userQueryOne,
 };
 
 export { egratitudeProduction };
